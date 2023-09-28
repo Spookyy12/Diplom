954b780f0405870a7cd5a4ab094fbcdde9ff3ca3
Spookyy12 <131680995+Spookyy12@users.noreply.github.com>
2023-09-28 19:11:05 +0300

    writeTest

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,19 +1,181 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardTest {
     MainPage mainPage = new MainPage();
+    DataHelper dataHelper;
+
     @BeforeEach
-    void setup (){
+    void setup() {
         open("http://localhost:8080/");
+        mainPage.buttonReady.click();
     }
 
     @Test
-    public void validTest(){
-        mainPage.validCardForString();
+    @DisplayName("Оплата по активной карте. Валидные данные")
+    public void approvedCardTest() {
+        var user = DataHelper.userApprovedCard();
+        mainPage.cardHolderNumber.setValue(user.getCardNumber());
+        mainPage.cardHolderMonthDate.setValue(user.getMonth());
+        mainPage.cardHolderYearDate.setValue(user.getYear());
+        mainPage.cardHolderName.setValue(user.getCardName());
+        mainPage.cardHolderCVC.setValue(user.getCvcCode());
+        mainPage.buttonNext.click();
+        mainPage.textCorrect.shouldBe(visible, Duration.ofSeconds(20)).
+                shouldHave(Condition.exactText("Операция одобрена Банком."));
     }
+
+/*    @Test
+    public void declinedCardTest() {
+        var user = DataHelper.userDeclinedCard();
+        mainPage.cardHolderNumber.setValue(user.getCardNumber());
+        mainPage.cardHolderMonthDate.setValue(user.getMonth());
+        mainPage.cardHolderYearDate.setValue(user.getYear());
+        mainPage.cardHolderName.setValue(user.getCardName());
+        mainPage.cardHolderCVC.setValue(user.getCvcCode());
+        mainPage.buttonNext.click();
+        mainPage.textCorrect.shouldBe(visible, Duration.ofSeconds(10)).
+                shouldHave(Condition.exactText("Карта заблокирована"));
+    }*/
+    @Test
+    public void dataFormEmptyNumberCard() {
+        var user = DataHelper.userApprovedCard();
+        mainPage.cardHolderMonthDate.setValue(user.getMonth());
+        mainPage.cardHolderYearDate.setValue(user.getYear());
+        mainPage.cardHolderName.setValue(user.getCardName());
+        mainPage.cardHolderCVC.setValue(user.getCvcCode());
+        mainPage.buttonNext.click();
+        mainPage.errorFormatFromCard.shouldHave(Condition.exactText("Неверный формат"));
+    }
+    @Test
+    public void dataFormEmptyMonth() {
+        var user = DataHelper.userApprovedCard();
+        mainPage.cardHolderNumber.setValue(user.getCardNumber());
+        mainPage.cardHolderYearDate.setValue(user.getYear());
+        mainPage.cardHolderName.setValue(user.getCardName());
+        mainPage.cardHolderCVC.setValue(user.getCvcCode());
+        mainPage.buttonNext.click();
+        mainPage.errorFormatFromMonth.shouldHave(Condition.exactText("Неверный формат"));
+    }
+    @Test
+    @DisplayName("Оплата по активной карте. Валидные данные")
+    public void dataFormEmptyYear() {
+        var user = DataHelper.userApprovedCard();
+        mainPage.cardHolderNumber.setValue(user.getCardNumber());
+        mainPage.cardHolderMonthDate.setValue(user.getMonth());
+        mainPage.cardHolderName.setValue(user.getCardName());
+        mainPage.cardHolderCVC.setValue(user.getCvcCode());
+        mainPage.buttonNext.click();
+        mainPage.errorFormatFromYear.shouldHave(Condition.exactText("Неверный формат"));
+    }
+    @Test
+    @DisplayName("Оплата по активной карте. Валидные данные")
+    public void dataFormEmptyName() {
+        var user = DataHelper.userApprovedCard();
+        mainPage.cardHolderNumber.setValue(user.getCardNumber());
+        mainPage.cardHolderYearDate.setValue(user.getYear());
+        mainPage.cardHolderMonthDate.setValue(user.getMonth());
+        mainPage.cardHolderCVC.setValue(user.getCvcCode());
+        mainPage.buttonNext.click();
+        mainPage.errorFormatFromName.shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+    @Test
+    public void dataFormEmptyCvc() {
+        var user = DataHelper.userApprovedCard();
+        mainPage.cardHolderNumber.setValue(user.getCardNumber());
+        mainPage.cardHolderMonthDate.setValue(user.getMonth());
+        mainPage.cardHolderYearDate.setValue(user.getYear());
+        mainPage.cardHolderName.setValue(user.getCardName());
+        mainPage.buttonNext.click();
+        mainPage.errorFormatFromCvc.shouldHave(Condition.exactText("Неверный формат"));
+    }
+    @Test
+    public void dataWrongCard() {
+        var user = DataHelper.userApprovedCard();
+        mainPage.cardHolderNumber.setValue("123");
+        mainPage.cardHolderMonthDate.setValue(user.getMonth());
+        mainPage.cardHolderYearDate.setValue(user.getYear());
+        mainPage.cardHolderName.setValue(user.getCardName());
+        mainPage.cardHolderCVC.setValue(user.getCvcCode());
+        mainPage.buttonNext.click();
+        mainPage.errorFormatFromCard.shouldHave(Condition.exactText("Неверный формат"));
+    }
+    @Test
+    public void dataWrongMonth() {
+        var user = DataHelper.userApprovedCard();
+        mainPage.cardHolderNumber.setValue(user.getCardNumber());
+        mainPage.cardHolderMonthDate.setValue("13");
+        mainPage.cardHolderYearDate.setValue(user.getYear());
+        mainPage.cardHolderName.setValue(user.getCardName());
+        mainPage.cardHolderCVC.setValue(user.getCvcCode());
+        mainPage.buttonNext.click();
+        mainPage.errorExpiredCardMonth.shouldHave(Condition.exactText("Неверно указан срок действия карты"));
+    }
+    @Test
+    public void dataWrongYear() {
+        var user = DataHelper.userApprovedCard();
+        mainPage.cardHolderNumber.setValue(user.getCardNumber());
+        mainPage.cardHolderMonthDate.setValue(user.getMonth());
+        mainPage.cardHolderYearDate.setValue("88");
+        mainPage.cardHolderName.setValue(user.getCardName());
+        mainPage.cardHolderCVC.setValue(user.getCvcCode());
+        mainPage.buttonNext.click();
+        mainPage.errorYear.shouldHave(Condition.exactText("Неверно указан срок действия карты"));
+    }
+    @Test     //Баг репорт при дате и годе Неверно указан срок действия карты
+    //
+    public void dataExpiredCard() {
+        var user = DataHelper.userApprovedCard();
+        mainPage.cardHolderNumber.setValue(user.getCardNumber());
+        mainPage.cardHolderMonthDate.setValue(user.getMonth());
+        mainPage.cardHolderYearDate.setValue("22");
+        mainPage.cardHolderName.setValue(user.getCardName());
+        mainPage.cardHolderCVC.setValue(user.getCvcCode());
+        mainPage.buttonNext.click();
+        mainPage.errorExpiredCardYear.shouldHave(Condition.exactText("Истёк срок действия карты"));
+    }
+
+    @Test  //Баг репорт имя на русской
+    public void dataWrongNameLanguage() {
+        var user = DataHelper.userApprovedCard();
+        mainPage.cardHolderNumber.setValue(user.getCardNumber());
+        mainPage.cardHolderMonthDate.setValue(user.getMonth());
+        mainPage.cardHolderYearDate.setValue(user.getYear());
+        mainPage.cardHolderName.setValue("Привет Привет");
+        mainPage.cardHolderCVC.setValue(user.getCvcCode());
+        mainPage.buttonNext.click();
+        mainPage.errorYear.shouldHave(Condition.exactText("Неверный формат"));
+    }
+    @Test //Баг репорт имя цифрами
+    public void dataWrongNameWriteNumbers() {
+        var user = DataHelper.userApprovedCard();
+        mainPage.cardHolderNumber.setValue(user.getCardNumber());
+        mainPage.cardHolderMonthDate.setValue(user.getMonth());
+        mainPage.cardHolderYearDate.setValue(user.getYear());
+        mainPage.cardHolderName.setValue("123 123");
+        mainPage.cardHolderCVC.setValue(user.getCvcCode());
+        mainPage.buttonNext.click();
+        mainPage.errorYear.shouldHave(Condition.exactText("Неверный формат"));
+    }
+    @Test
+    public void dataWrongCvc() {
+        var user = DataHelper.userApprovedCard();
+        mainPage.cardHolderNumber.setValue(user.getCardNumber());
+        mainPage.cardHolderMonthDate.setValue(user.getMonth());
+        mainPage.cardHolderYearDate.setValue(user.getYear());
+        mainPage.cardHolderName.setValue(user.getCardName());
+        mainPage.cardHolderCVC.setValue("22");
+        mainPage.buttonNext.click();
+        mainPage.errorExpiredCvc.shouldHave(Condition.exactText("Неверный формат"));
+    }
+
 }
--- src/test/java/DataHelper.java
+++ src/test/java/DataHelper.java
@@ -1,35 +1,54 @@
+import com.github.javafaker.CreditCardType;
 import com.github.javafaker.Faker;
 import lombok.Value;
 
 
-import java.text.SimpleDateFormat;
-import java.time.Instant;
+import java.security.Security;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class DataHelper {
     DataHelper() {
     }
-
     private static final Faker faker = new Faker(new Locale("en"));
-    static SimpleDateFormat sdf = new SimpleDateFormat("MM");
-
-
-        public static UserCard getAuthInfo() {
-            return new UserCard(faker.finance().creditCard(), sdf.format(faker.date().birthday()),
-                    faker.random().nextInt(23, 28), faker.random().nextInt(100, 999), faker.name().fullName());
-        }
-
-        @Value
-        public static class UserCard {
-            private String numberCard;
-            private String month;
-            private int year;
-            private int numberCvc;
-            private String cardName;
-
-        }
-
+    private static String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern((pattern)));
     }
+    static String monthGenerate = generateDate(1, "MM");
+    static String yearGenerate = generateDate(0, "yy");
+
+    public static UserCardApproved userApprovedCard() {
+        return new UserCardApproved(cvc,"1111 2222 3333 4444",monthGenerate,
+                yearGenerate, faker.name().fullName());
+    }
+    public static UserCardApproved userDeclinedCard() {
+        return new UserCardApproved(cvc,"5555 6666 7777 8888",monthGenerate,
+                yearGenerate, faker.name().fullName());
+    }
+    @Value
+    public static class UserCardApproved {
+        private String cvcCode;
+        private String cardNumber;
+        private String month;
+        private String year;
+        private String cardName;
+    }
+    @Value
+    public static class UserCardDeclined {
+        private String cvcCode;
+        private String cardNumber;
+        private String month;
+        private String year;
+        private String cardName;
+    }
+
+    private static String randomCVC() {
+        int randomNumber = (int) (Math.random() * 9);
+        int randomNumber1 = (int) (Math.random() * 9);
+        int randomNumber2 = (int) (Math.random() * 9);
+        return randomNumber + "" + randomNumber1 +""+randomNumber2 ;
+    }
+    static String cvc = randomCVC();
+
+}
